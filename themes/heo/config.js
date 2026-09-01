91b4a018fe60734011ebacb8c8f5e6e426de6de0
alex30888 <3088862195@qq.com>
2026-09-01 11:23:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -35,7 +35,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享知识',
-  HEO_HERO_TITLE_2: '与日常生活',
+  HEO_HERO_TITLE_2: '记录生活',
   HEO_HERO_TITLE_3: '268898.xyz',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'XiaoChao',
